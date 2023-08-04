3a2f51421a183824c31bceb2e9563ffdfa64908a
Annamalai1995 <s.annamalai1995@gmail.com>
2023-08-04 10:22:21 +0530

    vicky stack bs

---
+++ BS1/javascript/FunctionJs.js
@@ -0,0 +1,17 @@
+function data()
+{
+    alert("This is normal function using in javascript")
+}
+function data1(name,collegename)
+{
+    alert("Name is"+name+":Collegename is"+collegename)
+}
+var exe=()=>
+{
+    document.writeln("This is FAt Arrow Function ")
+}
+const multiple=(a,b)=>
+{
+    return a*b;
+
+}
